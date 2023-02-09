0904f3fe407ad4d8239f75b961c8381d85d313de
Luis Felipe Pizzi Lopes <101680647+luispizzilopes@users.noreply.github.com>
2023-02-09 15:02:19 -0300

    Update file Create-database-BdLogin

--- database/Create-database-BdLogin.sql
+++ database/Create-database-BdLogin.sql
@@ -3,6 +3,6 @@ CREATE DATABASE [BdLogin]
 GO
 USE BdLogin
 CREATE TABLE [dbo].[TabelaLogin](
-	[username] [varchar](50) NOT NULL,
-	[password] [varchar](50) NOT NULL
+	[Login] [varchar](50) NOT NULL,
+	[Senha] [varchar](50) NOT NULL
 ) ON [PRIMARY]
